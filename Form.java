4de4ec3320a8e8ace6e974d31d84ef5dc19c3a4a
Jayasri <jayasri.suresh2021@gmail.com>
2023-10-02 13:25:08 +0530

    form-page

--- Form.java
+++ Form.java
@@ -17,3 +17,17 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
+public class Form extends MFS100Test {
+    private static final long serialVersionUID = 1L;
+    // initialize text fields
+    private JPanel contentPane;
+    private JTextField uname;
+    private JTextField udob;
+    private JTextField uemail;
+    private JTextField uage;
+    private JTextField uaddr;
+    private JTextField uphno;
+    private JButton usubmit;
+    private JButton ucapture;
+    // private JButton uaddr1;
+
